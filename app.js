9faed153e0ae64eab41b66533ebdd882743139bb
dristanovic <157993649+dristanovic@users.noreply.github.com>
2024-04-17 21:09:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,52 +4,59 @@ const tabla = document.querySelector("#tabla");
 const klijent = supabase.createClient("https://vcqzganpthronrmdviox.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjcXpnYW5wdGhyb25ybWR2aW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk1NzkwMzcsImV4cCI6MjAyNTE1NTAzN30.VEXCtvc3WLeO6R909CEfE7UKkcVq1quQu9269HRJOgM");
 const kod = new URLSearchParams(window.location.search).get("id");
 
+document.querySelector("input").value = kod;
+document.querySelector("input").setAttribute("size", kod.toString().length);
+
 async function inicijalizuj() {
   if (!localStorage.getItem(kod)) localStorage.setItem(kod, (await klijent.rpc("slobodan_oblik", { id: kod })).data); // u lokalnoj memoriji cuvamo oblik ovog klijenta
   const potez = localStorage.getItem(kod);
   tabla.classList.add("igrac-" + potez);
+  document.querySelector("#moj-oblik").classList.add(potez);
   postaviPoziciju((await klijent.from("iks_oks").select("pozicija").eq("id", kod).limit(1).single()).data.pozicija);
 
   const maliKvadrati = document.querySelectorAll(".mali.kvadrat");
-  // svim malim kvadratima dodeljujemo funkciju koja će obrađivati klik
+  // svim malim kvadratima dodeljujemo funkciju koja ce obradjivati klik
   maliKvadrati.forEach((maliKvadrat) => {
     maliKvadrat.addEventListener("click", async function klik() {
       if (maliKvadrat.parentElement.classList.contains("potez-" + potez)) {
         maliKvadrat.classList.add(potez);
-        proveriKvadrat(maliKvadrat, potez);
         proveriKvadrat(maliKvadrat.parentElement, potez);
+        proveriKvadrat(tabla, potez);
         resetujKvadrate();
 
-        // proveravamo da li je partija gotova, ako nije aktiviramo sledece velike kvadrate
-        if (!tabla.classList.contains("iks") && !tabla.classList.contains("oks") && document.querySelectorAll(".veliki.kvadrat.iks , .veliki.kvadrat.oks").length !== 9) sledeciKvadrati(maliKvadrat);
-        maliKvadrat.removeEventListener("click", klik); // kada kliknemo na kvadrat ne možemo ga više koristiti
-        
-        const { data, error } = await klijent.rpc("napravi_potez", { id: kod, nova_pozicija: pozicija() });
+        if (!tabla.classList.contains("iks") && !tabla.classList.contains("oks") && document.querySelectorAll(".veliki.iks , .veliki.oks").length < 9) sledeciKvadrati(maliKvadrat);
+
+        // obaveštavamo bazu podataka da je potez odigran
+        await klijent.rpc("napravi_potez", {
+          id: kod,
+          nova_pozicija: pozicija(),
+        });
+        maliKvadrat.removeEventListener("click", klik);
       }
     });
   });
 }
 inicijalizuj();
 
-const changes = klijent
+const promene = klijent
   .channel("promena-pozicije")
   .on(
     "postgres_changes",
     {
-      event: "UPDATE",
+      event: "UPDATE", // na svaki UPDATE
       schema: "public",
-      table: "iks_oks",
-      filter: "id=eq." + kod,
+      table: "iks_oks", // nad tabelom iks_oks
+      filter: "id=eq." + kod, // gde je id jednak kodu partije
     },
-    (data) => postaviPoziciju(data.new.pozicija)
+    (data) => postaviPoziciju(data.new.pozicija) // postavljamo novu poziciju
   )
   .subscribe();
 
 function aktivirajKvadrat(kvadrat, potez) {
   kvadrat.classList.add("potez-" + potez);
 }
+
 function proveriKvadrat(kvadrat, potez) {
-  kvadrat = kvadrat.parentElement;
   if (kvadrat.querySelectorAll(":scope > .gore." + potez).length === 3) kvadrat.classList.add(potez);
   else if (kvadrat.querySelectorAll(":scope > .centar." + potez).length === 3) kvadrat.classList.add(potez);
   else if (kvadrat.querySelectorAll(":scope > .dole." + potez).length === 3) kvadrat.classList.add(potez);
@@ -66,32 +73,18 @@ function resetujKvadrate() {
   });
 }
 function sledeciKvadrati(maliKvadrat) {
-  let velikiKvadrat;
-  if (maliKvadrat.classList.contains("gore")) {
-    if (maliKvadrat.classList.contains("levo")) velikiKvadrat = document.querySelector(".veliki.kvadrat.gore.levo");
-    else if (maliKvadrat.classList.contains("desno")) velikiKvadrat = document.querySelector(".veliki.kvadrat.gore.desno");
-    else velikiKvadrat = document.querySelector(".veliki.kvadrat.gore.sredina");
-  } else if (maliKvadrat.classList.contains("centar")) {
-    if (maliKvadrat.classList.contains("levo")) velikiKvadrat = document.querySelector(".veliki.kvadrat.centar.levo");
-    else if (maliKvadrat.classList.contains("desno")) velikiKvadrat = document.querySelector(".veliki.kvadrat.centar.desno");
-    else velikiKvadrat = document.querySelector(".veliki.kvadrat.centar.sredina");
-  } else {
-    if (maliKvadrat.classList.contains("levo")) velikiKvadrat = document.querySelector(".veliki.kvadrat.dole.levo");
-    else if (maliKvadrat.classList.contains("desno")) velikiKvadrat = document.querySelector(".veliki.kvadrat.dole.desno");
-    else velikiKvadrat = document.querySelector(".veliki.kvadrat.dole.sredina");
-  }
+  const velikiKvadrat = document.querySelector(`.veliki.kvadrat[data-index="${maliKvadrat.dataset.index}"]`);
 
   let potez;
   if (maliKvadrat.classList.contains("iks")) potez = "oks";
   else potez = "iks";
   if (velikiKvadrat.classList.contains("iks") || velikiKvadrat.classList.contains("oks") || velikiKvadrat.querySelectorAll(".iks, .oks").length === 9) {
-    document.querySelectorAll(".veliki.kvadrat:not(.iks, .oks)").forEach((velikiKvadrat) => aktivirajKvadrat(velikiKvadrat, potez));
+    document.querySelectorAll(".veliki.kvadrat:not(.iks, .oks)").forEach((velikiKvadrat) => {
+      if (velikiKvadrat.querySelectorAll(".iks, .oks").length < 9) aktivirajKvadrat(velikiKvadrat, potez);
+    });
   } else aktivirajKvadrat(velikiKvadrat, potez);
 }
-/*
 
-
-*/
 function pozicija() {
   let pozicija = "";
   document.querySelectorAll(".kvadrat").forEach((kvadrat) => {
@@ -109,23 +102,31 @@ function pozicija() {
 }
 function postaviPoziciju(pozicija) {
   resetujKvadrate();
+
   pozicija = pozicija.replaceAll(" ", "").replaceAll("=", "");
-  document.querySelectorAll(".kvadrat").forEach((kvadrat, index) => postaviKvadrat(kvadrat, pozicija[index]));
 
-  function postaviKvadrat(kvadrat, opcija) {
-    if (opcija === "x") kvadrat.classList.add("potez-iks");
-    else if (opcija === "o") kvadrat.classList.add("potez-oks");
-    else if (opcija === "X") kvadrat.classList.add("iks");
-    else if (opcija === "O") kvadrat.classList.add("oks");
-  }
+  document.querySelectorAll(".kvadrat").forEach((kvadrat, index) => {
+    if (pozicija[index] === "x") kvadrat.classList.add("potez-iks");
+    else if (pozicija[index] === "o") kvadrat.classList.add("potez-oks");
+    else if (pozicija[index] === "X") kvadrat.classList.add("iks");
+    else if (pozicija[index] === "O") kvadrat.classList.add("oks");
+  });
+  proveriKvadrat(tabla, "iks");
+  proveriKvadrat(tabla, "oks");
+}
 
-  /*const velikiKvadrati = document.querySelectorAll(".veliki.kvadrat");
-  pozicija.split(" ").forEach((pozicijaVelikogKvadrata, index) => {
-    postaviKvadrat(velikiKvadrati[index], pozicijaVelikogKvadrata[0]);
-
-    const pozicijaMalihKvadrata = pozicijaVelikogKvadrata.split("=")[1];
-    for (let i = 0; i < pozicijaMalihKvadrata.length; i++) {
-      postaviKvadrat(velikiKvadrati[index].children[i], pozicijaMalihKvadrata[i]);
-    }
-  });*/
+function kreirajQR() {
+  const div = document.createElement("div");
+  div.setAttribute("id", "qr-kod");
+  div.setAttribute("onclick", "this.remove();");
+  document.querySelector("body").append(div);
+  const velicina = Math.min(window.innerWidth, window.innerHeight) * 0.9;
+  new QRCode(div, {
+    text: window.location.href,
+    width: velicina,
+    height: velicina,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.L,
+  });
 }
